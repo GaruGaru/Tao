c3a3b11f3a8c86a7099ba954c92b213a09171570
GaruGaru <garuglieritommaso@gmail.com>
2018-03-11 21:21:06 +0100

    Cache refactor with interface, redis and local cache

--- providers/cached_provider.go
+++ providers/cached_provider.go
@@ -1,29 +1,21 @@
 package providers
 
 import (
-	"strings"
 	"strconv"
 	"github.com/cactus/go-statsd-client/statsd"
-	"github.com/go-redis/redis"
-	"encoding/json"
-	"time"
-	"fmt"
-	log "github.com/sirupsen/logrus"
 )
 
 type CachedEventsProvider struct {
-	Provider      EventProvider
-	statsd        statsd.Statter
-	redis         redis.Client
-	cacheDuration time.Duration
+	Provider EventProvider
+	statsd   statsd.Statter
+	Cache    []EventsCache
 }
 
-func NewCachedEventsProvider(provider EventProvider, redis redis.Client, cacheDuration time.Duration, statsd statsd.Statter) CachedEventsProvider {
+func NewCachedEventsProvider(provider EventProvider, caches []EventsCache, statsd statsd.Statter) CachedEventsProvider {
 	return CachedEventsProvider{
-		Provider:      provider,
-		statsd:        statsd,
-		redis:         redis,
-		cacheDuration: cacheDuration,
+		Provider: provider,
+		statsd:   statsd,
+		Cache:    caches,
 	}
 }
 
@@ -31,58 +23,39 @@ func (p CachedEventsProvider) Events(lat float64, lon float64, rng int, sorting 
 
 	key := requestKey(lat, lon, rng, sorting)
 
-	cacheStart := time.Now()
-	cache, err := p.redis.Get(key).Result()
-	p.statsd.TimingDuration("cache.latency", time.Since(cacheStart), 1)
-
-	if err == redis.Nil || err != nil { // redis.Nil->Key does not exists
-		log.Info("Cache miss for key %s", key)
-		p.statsd.Inc("cache.miss", 1, 1)
-
-		apiStart := time.Now()
-		events, err := p.Provider.Events(lat, lon, rng, sorting)
-		p.statsd.TimingDuration("api.latency", time.Since(apiStart), 1)
-
-		if err != nil {
-			return nil, err
+	for _, cache := range p.Cache {
+		result, present, err := cache.Get(lat, lon, rng, sorting)
+		if err == nil && present {
+			p.statsd.Inc("cache.hit."+cache.Name(), 1, 1.0)
+			return result, nil
+		} else if err != nil {
+			p.statsd.Inc("cache.error."+cache.Name(), 1, 1.0)
 		}
-
-		p.updateCache(events, key)
-
-		return events, nil
-
-	} else {
-		log.Info("Cache hit for key %s", key)
-		p.statsd.Inc("cache.hit", 1, 1)
-		var events []DojoEvent
-		json.Unmarshal([]byte(cache), &events)
-		return events, nil
+		p.statsd.Inc("cache.miss." + cache.Name(), 1, 1.0)
 	}
 
+	events, err := p.Provider.Events(lat, lon, rng, sorting)
+
+	if err != nil {
+		return nil, err
+	}
+
+	p.updateCache(events, key)
+
+	return events, nil
+
 }
+
 func (p CachedEventsProvider) updateCache(events []DojoEvent, key string) {
-	eventsJson, err := json.Marshal(events)
-	if err == nil {
-
-		res := p.redis.Set(key, string(eventsJson), p.cacheDuration)
-		if res.Err() != nil {
-			log.Warn("Unable to Set cache for %d events with key %s error: %s\n", len(events), key, res.Err().Error())
-		} else {
-			log.Info("Cache update for key %s", key)
-			p.statsd.Inc("cache.update", 1, 1)
+	for _, cache := range p.Cache {
+		err := cache.Put(key, events)
+		if err != nil {
+			println(err.Error())
+			p.statsd.Inc("cache.update_error." + cache.Name(), 1, 1.0)
+		}else{
+			p.statsd.Inc("cache.update." + cache.Name(), 1, 1.0)
 		}
-
-	} else {
-		panic(err)
-		fmt.Printf("Unable to marshal %d events\n", len(events))
 	}
-}
-
-func requestKey(lat float64, lon float64, rng int, sorting string) string {
-	return strings.Join(
-		[]string{float64ToString(lat), float64ToString(lon), strconv.Itoa(rng), sorting},
-		":",
-	)
 }
 
 func float64ToString(value float64) string {
