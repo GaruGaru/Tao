caac4419b84a4bcdeb17fa47384e0070b64d3bd9
Tommaso Garuglieri <garuglieritommaso@gmail.com>
2018-03-12 12:13:32 +0100

    Added statsd to cached provider

--- providers/cached_provider.go
+++ providers/cached_provider.go
@@ -3,6 +3,8 @@ package providers
 import (
 	"strconv"
 	"github.com/cactus/go-statsd-client/statsd"
+	"fmt"
+	"time"
 )
 
 type CachedEventsProvider struct {
@@ -24,23 +26,36 @@ func (p CachedEventsProvider) Events(lat float64, lon float64, rng int, sorting 
 	key := requestKey(lat, lon, rng, sorting)
 
 	for _, cache := range p.Cache {
+		cacheFetchStart := time.Now()
 		result, present, err := cache.Get(lat, lon, rng, sorting)
 		if err == nil && present {
-			p.statsd.Inc("cache.hit."+cache.Name(), 1, 1.0)
+			p.statsd.Inc(fmt.Sprintf("cache.%s.hit", cache.Name()), 1, 1.0)
+			p.statsd.TimingDuration(fmt.Sprintf("cache.%s.latency", cache.Name()), time.Now().Sub(cacheFetchStart), 1.0)
 			return result, nil
 		} else if err != nil {
-			p.statsd.Inc("cache.error."+cache.Name(), 1, 1.0)
+			p.statsd.Inc(fmt.Sprintf("cache.%s.error", cache.Name()), 1, 1.0)
 		}
-		p.statsd.Inc("cache.miss." + cache.Name(), 1, 1.0)
+		p.statsd.Inc(fmt.Sprintf("cache.%s.miss", cache.Name()), 1, 1.0)
 	}
+
+	fetchStart := time.Now()
 
 	events, err := p.Provider.Events(lat, lon, rng, sorting)
 
+	p.statsd.TimingDuration("fetch.latency", time.Now().Sub(fetchStart), 1.0)
+
+
 	if err != nil {
+		p.statsd.Inc("fetch.error", 1, 1.0)
 		return nil, err
 	}
 
+	p.statsd.Inc("fetch.ok", 1, 1.0)
+
+	cacheUpdateStart := time.Now()
 	p.updateCache(events, key)
+	p.statsd.TimingDuration("cache.update.latency", time.Now().Sub(cacheUpdateStart), 1.0)
+
 
 	return events, nil
 
@@ -51,9 +66,9 @@ func (p CachedEventsProvider) updateCache(events []DojoEvent, key string) {
 		err := cache.Put(key, events)
 		if err != nil {
 			println(err.Error())
-			p.statsd.Inc("cache.update_error." + cache.Name(), 1, 1.0)
-		}else{
-			p.statsd.Inc("cache.update." + cache.Name(), 1, 1.0)
+			p.statsd.Inc(fmt.Sprintf("cache.%s.update.error", cache.Name()), 1, 1.0)
+		} else {
+			p.statsd.Inc(fmt.Sprintf("cache.%s.update.ok", cache.Name()), 1, 1.0)
 		}
 	}
 }
